c74cf3faae381b44574923453f97d7b0b7856cfa
EpanovDenis <epanov.ewar@gmail.com>
2022-08-05 22:42:31 +0300

    test_BearsComGames

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,21 +21,53 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {                     
-        if (moveInputY.y == 0)
+    {
+        if (moveInputX.x < 0)
         {
+            PlayerRB.rotation = 180f;
+        }
+        if (moveInputX.x > 0)
+        {
+            PlayerRB.rotation = 0f;
+        }
+
+        if (moveInputY.y == 0)
+        {           
             if (_inverted)
-            {
+            {                
                 PlayerRB.MovePosition(PlayerRB.position - moveInputX * _movementSpeedPlayer * Time.fixedDeltaTime);
             }
             else
-            {
+            {            
                 PlayerRB.MovePosition(PlayerRB.position + moveInputX * _movementSpeedPlayer * Time.fixedDeltaTime);
             }
         }
         if (moveInputX.x == 0)
         {
             PlayerRB.velocity = moveInputY * _movementSpeedPlayer;
+
+            if (_inverted)
+            {
+                if (moveInputY.y < 0)
+                {
+                    PlayerRB.rotation = 90f;
+                }
+                if (moveInputY.y > 0)
+                {
+                    PlayerRB.rotation = -90f;
+                }
+            }
+            else
+            {
+                if (moveInputY.y > 0)
+                {
+                    PlayerRB.rotation = 90f;
+                }
+                if (moveInputY.y < 0)
+                {
+                    PlayerRB.rotation = -90f;
+                }
+            }            
         }        
     }
 }
